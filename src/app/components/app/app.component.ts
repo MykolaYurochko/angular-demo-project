a503777303de139e23d17d72cdcd0eec880d6209
MykolaYurochko <koljaa94@gmail.com>
2020-06-30 21:40:45 +0300

    - Posts done!

--- src/app/components/app/app.component.ts
+++ src/app/components/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { User } from '../../models/User';
+import { Post } from '../../models/Post';
 @Component({
   selector: 'app-root',
-  template: `<h2>Hello</h2>
-    <app-user *ngFor="let u of users" [user]="u"></app-user>`,
+  template: `<h2>Users</h2>
+    <app-user *ngFor="let user of users" [user]="user"></app-user>
+    <hr />
+    <h2>Posts</h2>
+    <app-post *ngFor="let post of posts" [post]="post"></app-post>`,
   styles: [
     `
       h2 {
@@ -15,7 +19,7 @@ import { User } from '../../models/User';
 })
 export class AppComponent {
   users: User[];
-  posts: any[];
+  posts: Post[];
   comments: any[];
 
   constructor(private httpClient: HttpClient) {
@@ -24,7 +28,7 @@ export class AppComponent {
       .subscribe((data) => (this.users = data));
 
     this.httpClient
-      .get<any[]>('https://jsonplaceholder.typicode.com/posts')
+      .get<Post[]>('https://jsonplaceholder.typicode.com/posts')
       .subscribe((data) => (this.posts = data));
 
     this.httpClient
